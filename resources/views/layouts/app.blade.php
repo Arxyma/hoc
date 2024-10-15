976d653a4b5db5b8ba08b092a14c242b91d87b6a
Arxyma <ilham.arsyam@gmail.com>
2024-10-15 08:45:26 +0700

    update role system & fix gitignore

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Hall of Community') }}</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
