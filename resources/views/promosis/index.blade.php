038c335e1bc4b505160b149d3dc4b1a1be37eb75
yusufalharits <yusufalharits2515@gmail.com>
2024-11-08 09:08:04 +0700

    edit tampilan admin, createe

--- resources/views/promosis/index.blade.php
+++ resources/views/promosis/index.blade.php
@@ -23,6 +23,7 @@
                  @endcan
             </div>
             
+            
     
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
                 @foreach($promosis as $promosi)
