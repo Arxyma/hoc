4bf35c30ebb1c4b805a7659378f8c9d23659be70
Moscart <danieltheo.73@gmail.com>
2024-11-11 11:57:36 +0700

    refactor: remove comment

--- resources/views/promosis/index.blade.php
+++ resources/views/promosis/index.blade.php
@@ -23,69 +23,6 @@
     <section class="max-w-screen-xl mx-auto px-6 mt-20">
         <div class="grid grid-cols-1 max-w-md md:max-w-full mx-auto md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
             @foreach ($promosis as $promosi)
-                {{-- <div class="bg-white shadow-md rounded-lg overflow-hidden p-4">
-                    <div class="h-48 overflow-hidden mb-4">
-                        @if ($promosi->foto_produk)
-                            @php
-                                $foto_produk = json_decode($promosi->foto_produk);
-                                $foto_pertama = $foto_produk[0] ?? null;
-                            @endphp
-                            @if ($foto_pertama)
-                                <img src="{{ asset('storage/' . $foto_pertama) }}" alt="Foto Produk"
-                                    class="w-full h-full object-cover">
-                            @else
-                                <div class="w-full h-full bg-gray-200 flex items-center justify-center text-gray-500">
-                                    No Image
-                                </div>
-                            @endif
-                        @else
-                            <div class="w-full h-full bg-gray-200 flex items-center justify-center text-gray-500">
-                                No Image
-                            </div>
-                        @endif
-                    </div>
-                    <div>
-                        <h2 class="text-lg font-semibold mb-2">
-                            <a href="{{ route('promosis.detail', $promosi->id) }}"
-                                class="text-blue-500 hover:underline">
-                                {{ $promosi->judul }}
-                            </a>
-                        </h2>
-                        <div class="text-gray-600 flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 24 24"
-                                fill="currentColor">
-                                <path
-                                    d="M12 2a5 5 0 100 10 5 5 0 000-10zM4 18a8 8 0 0116 0v1a1 1 0 01-1 1H5a1 1 0 01-1-1v-1z" />
-                            </svg>
-                            <span>{{ $promosi->user->name ?? 'Unknown' }}</span>
-                        </div>
-
-                        <div class="text-gray-500 text-sm flex items-center mt-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 24 24"
-                                fill="currentColor">
-                                <path
-                                    d="M8 2a1 1 0 00-1 1v1H5a3 3 0 00-3 3v12a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3h-2V3a1 1 0 10-2 0v1H9V3a1 1 0 00-1-1zM4 10h16v9a1 1 0 01-1 1H5a1 1 0 01-1-1v-9z" />
-                            </svg>
-                            <span>{{ $promosi->created_at->diffForHumans() }}</span>
-                        </div>
-
-                        @can('admin')
-                            <td class="border px-4 py-2">
-                                <form action="{{ route('promosis.edit', $promosi->id) }}" method="GET"
-                                    class="inline-block">
-                                    <button type="submit" class="bg-yellow-500 text-white px-2 py-1 rounded">Edit</button>
-                                </form>
-                                <form action="{{ route('promosis.destroy', $promosi->id) }}" method="POST"
-                                    class="inline-block" onsubmit="return confirmDelete()">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit"
-                                        class="bg-red-500 text-white px-2 py-1 mt-2 rounded">Delete</button>
-                                </form>
-                            </td>
-                        @endcan
-                    </div>
-                </div> --}}
                 <div class="">
                     <a href="{{ route('promosis.detail', $promosi->id) }}"
                         class="bg-white shadow border rounded-xl overflow-hidden aspect-[5/4] relative block">
