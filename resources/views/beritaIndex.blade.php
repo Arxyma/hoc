c0b7b9f8349a70e7714ee0c34ea99834aa40d040
AdiPrstyo <loyaltythree10@gmail.com>
2024-11-19 11:21:02 +0700

    penyesuaian tampilan

--- resources/views/beritaIndex.blade.php
+++ resources/views/beritaIndex.blade.php
@@ -1,5 +1,10 @@
 <x-app-layout>
-    <section class="max-w-screen-xl mx-auto px-6 mt-20">
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tightx">
+            Berita
+        </h2>
+    </x-slot>
+    <section class="max-w-screen-xl mx-auto px-6 mt-10">
         <div class="container mx-auto px-10 py-12 bg-white rounded-xl ">
             <h1 class="text-3xl font-bold text-blue-500 lg:text-md text-center">Daftar Berita</h1>
             @if ($beritas->isEmpty())
@@ -9,29 +14,28 @@
             @else
                 <div class="grid grid-cols-1 max-w-md md:max-w-full mx-auto md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
                     @foreach ($beritas as $berita)
-                        <div
+                        <a href="{{ route('beritaTampil', $berita->slug) }}"
                             class="bg-white rounded-lg shadow-md overflow-hidden hover:scale-105 transition-transform duration-300 group">
+
                             @if ($berita->gambar)
                                 <img src="{{ asset('storage/' . $berita->gambar) }}" alt="Gambar Berita"
                                     class="w-full h-48 object-cover ">
                             @endif
                             <div class="p-4">
                                 <h2 class="text-lg font-semibold">{{ $berita->judul }}</h2>
-                                <p class="text-sm text-gray-500 mb-4">{{ $berita->slug }}</p>
+                                <p class="text-sm text-gray-500 mb-4">{{ $berita->created_at->format('d-m-Y ') }}</p>
                                 <p class="text-gray-600 mt-2">{{ Str::limit($berita->isi_berita, 55) }}
-                                    <a href="{{ route('beritaTampil', $berita->slug) }}"
-                                        class="text-blue-500 hover:underline inline-block">lihat selengkapnya</a>
                                 </p>
-
                             </div>
-                        </div>
+                        </a>
                     @endforeach
                 </div>
-            @endif
+        </div>
+        @endif
 
-            <div class="mt-4">
-                {{ $beritas->links() }}
-            </div>
+        <div class="mt-4">
+            {{ $beritas->links() }}
+        </div>
         </div>
     </section>
 
--- resources/views/beritaTampil.blade.php
+++ resources/views/beritaTampil.blade.php
@@ -9,16 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h3 class="text-2xl font-bold mb-4 text-center">{{ $berita->judul }}</h3>
-                    <p class="text-sm text-gray-600 mb-2"> {{ $berita->slug }}</p>
                     <p class="text-sm text-gray-600 mb-4">Dibuat pada: {{ $berita->created_at->format('d-m-Y ') }}</p>
-                    <div class="flex justify-center">
+                    <h3 class="text-2xl font-bold mb-4 text-center">{{ $berita->judul }}</h3>
+                    <div class="flex justify-center mb-6">
                         @if ($berita->gambar)
-                            <img src="{{ asset('storage/' . $berita->gambar) }}" alt="Gambar Berita" class="mb-4 w-1/4">
+                            <img src="{{ asset('storage/' . $berita->gambar) }}" alt="Gambar Berita" class="mb-4 w-1/2">
                         @endif
                     </div>
-                    <div class="text-gray-700 text-justify">
-                        <p>{{ $berita->isi_berita }}</p>
+                    <div class="text-gray-700 font-semibold text-justify leading-relaxed tracking-wide">
+                        <p class="text-lg">{{ $berita->isi_berita }}</p>
                     </div>
 
                 </div>
