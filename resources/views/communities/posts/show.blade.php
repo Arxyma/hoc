c6f6f48cd554078218d94ee07d153240d124bad5
Arxyma <ilham.arsyam@gmail.com>
2024-11-20 16:09:37 +0700

    fix login register, community comment sort by, button membership

--- resources/views/communities/posts/show.blade.php
+++ resources/views/communities/posts/show.blade.php
@@ -114,6 +114,25 @@
                                     <p class="mt-2 text-gray-600 dark:text-gray-400">{{ $comment->content }}</p>
                                 </div>
                             </div>
+                            <!-- Tombol Hapus Komentar -->
+                            @can('delete', $comment)
+                                <div class="w-1/5 lg:w-1/12 sm:w-5/6 flex justify-end">
+                                    <form action="{{ route('comments.destroy', $comment) }}" method="POST"
+                                        onsubmit="return confirm('Apakah Anda yakin ingin menghapus komentar ini?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit"
+                                            class="text-red-400 hover:text-red-600 transition duration-200 flex items-center">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none"
+                                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M19 7l-.867 12.142A2 2 0 0116.136 21H7.864a2 2 0 01-1.997-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3m7 0H5" />
+                                            </svg>
+                                            Hapus
+                                        </button>
+                                    </form>
+                                </div>
+                            @endcan
                         </div>
                     @endforeach
 
